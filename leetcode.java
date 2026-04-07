958c1b68893cdbaee7a9caf35abd989853df04eb
sujal <sujalchavan103@gmail.com>
2026-04-07 22:42:28 +0530

    next commit

--- leetcode.java
+++ leetcode.java
@@ -1,31 +1,39 @@
-import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 import java.io.*;
 
+class leetcode
+{
+	public static void main(String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 
-class Codechef {
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt(); // number of test cases
-        
-        while (T-- > 0) {
-            
-            String S = sc.next();
-            String Tstr = sc.next();
-            
-            StringBuilder M = new StringBuilder();
-            
-            for (int i = 0; i < 5; i++) {
-                if (S.charAt(i) == Tstr.charAt(i)) {
-                    M.append('G');
-                } else {
-                    M.append('B');
-                }
-            }
-            
-            System.out.println(M.toString());
-        }
-    }
+		while (T-- > 0)
+		{
+			int N = sc.nextInt();
+			String S = sc.next();
+			StringBuilder sb = new StringBuilder(S);
+            int count=0;
+
+			for (int i = 0; i < sb.length() - 1; i++)
+			{
+				if (sb.charAt(i) == sb.charAt(i + 1))
+				{
+					if (sb.charAt(i) == '0')
+					{
+						sb.insert(i + 1, '1');
+
+					}
+					else
+					{
+						sb.insert(i + 1, '0');
+					}
+                    count++;
+				}
+			}
+
+			System.out.println(count);
+		}
+	}
 }
